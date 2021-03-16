609a89e5eafe932a5f4cc8d0797e0cd65d593f47
Tomas Odstrcil <odstrcilt@fusion.gat.com>
2021-03-16 14:00:50 -0700

    saving bug

--- quickfit.py
+++ quickfit.py
@@ -1068,7 +1068,8 @@ class DataFit():
                         try:
                             settings[kin_prof]['load_options'][system][name] = options[0].get(), options[1]
                         except:
-                            printe('Error in saving',system,name, options  )
+                            printe(('Error in saving',system,name,var,  options  ))
+                            
         settings['EFIT'] = self.eqm.diag
         if len(data):
             self.OMFITsave.runNoGUI(shot=self.shot, fitted_profiles=data, setting=settings) 
