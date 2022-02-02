577523933f1b846e5e0485c4d512669ced230670
Tomas Odstrcil <odstrcilt@fusion.gat.com>
2022-02-02 13:43:38 -0800

    issue with sepx

--- D3D/fetch_data.py
+++ D3D/fetch_data.py
@@ -3133,11 +3133,10 @@ class data_loader:
             
             
             #rho coordinate of the horizontal line, used later for separatrix aligment 
-            print('CER sep location', 'horiz_cut' not in nimp , 'EQM' not in nimp , nimp['EQM']['id'] != id(self.eqm) , nimp['EQM']['ed'] != self.eqm.diag )
-            if 'horiz_cut' not in nimp or 'EQM' not in nimp or nimp['EQM']['id'] != id(self.eqm) or nimp['EQM']['ed'] != self.eqm.diag:
-                R = np.linspace(1.4,2.5,100)
-                rho_horiz = self.eqm.rz2rho(R, np.zeros_like(R), coord_out='rho_tor')
-                nimp['horiz_cut'] = {'time':self.eqm.t_eq, 'rho': np.single(rho_horiz), 'R':R}
+            #if 'horiz_cut' not in nimp or 'EQM' not in nimp or nimp['EQM']['id'] != id(self.eqm) or nimp['EQM']['ed'] != self.eqm.diag:
+            R = np.linspace(1.4,2.5,100)
+            rho_horiz = self.eqm.rz2rho(R, np.zeros_like(R), coord_out='rho_tor')
+            nimp['horiz_cut'] = {'time':self.eqm.t_eq, 'rho': np.single(rho_horiz), 'R':R}
             
             #build a new dictionary only with the requested and time sliced channels            
             nimp_out = {'systems':systems,'diag_names':Tree()}
